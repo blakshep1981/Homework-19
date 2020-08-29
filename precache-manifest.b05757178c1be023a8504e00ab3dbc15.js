28f8b192b907261784a69e085ae754f0c63d83ec
Ryan Simpkins <rythsimpkins@yahoo.com>
2020-08-29 11:27:34 -0700

    Updates

---
+++ precache-manifest.b05757178c1be023a8504e00ab3dbc15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e275d54f2ede9fbe834594b9944b491",
+    "url": "/Homework-19/index.html"
+  },
+  {
+    "revision": "1a51fca2adeb26af60c1",
+    "url": "/Homework-19/static/css/main.1e1c6ca6.chunk.css"
+  },
+  {
+    "revision": "4109fa1c3f339e030c03",
+    "url": "/Homework-19/static/js/2.cf8846ee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Homework-19/static/js/2.cf8846ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a51fca2adeb26af60c1",
+    "url": "/Homework-19/static/js/main.00db8bc4.chunk.js"
+  },
+  {
+    "revision": "c5dc47f0e01c06a22adf",
+    "url": "/Homework-19/static/js/runtime-main.6d80a6e9.js"
+  }
+]);
